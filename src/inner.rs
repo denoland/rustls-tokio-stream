64b1395f661f6edd1dc69d108a4a94965dd9ee3a
Matt Mastracci <matthew@mastracci.com>
2023-05-10 15:24:13 +0200

    feat: Initial commit imported from Deno plus test

---
+++ src/inner.rs
@@ -0,0 +1,296 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+use futures::ready;
+use futures::task::Context;
+use futures::task::Poll;
+
+use io::Error;
+use io::Read;
+use io::Write;
+use rustls::Connection;
+use std::convert::From;
+use std::io;
+use std::io::ErrorKind;
+use std::pin::Pin;
+use tokio::io::AsyncWrite;
+use tokio::io::ReadBuf;
+use tokio::net::TcpStream;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub(crate) enum Flow {
+    Handshake,
+    Read,
+    Write,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) enum State {
+    StreamOpen,
+    StreamClosed,
+    TlsClosing,
+    TlsClosed,
+    TcpClosed,
+}
+
+struct ImplementReadTrait<'a, T>(&'a mut T);
+
+impl Read for ImplementReadTrait<'_, TcpStream> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.try_read(buf)
+    }
+}
+
+struct ImplementWriteTrait<'a, T>(&'a mut T);
+
+impl Write for ImplementWriteTrait<'_, TcpStream> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self.0.try_write(buf) {
+            Ok(n) => Ok(n),
+            Err(err) if err.kind() == ErrorKind::WouldBlock => Ok(0),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub struct TlsStreamInner {
+    pub(crate) tls: Connection,
+    pub(crate) tcp: TcpStream,
+    pub(crate) rd_state: State,
+    pub(crate) wr_state: State,
+}
+
+impl TlsStreamInner {
+    pub(crate) fn poll_io(&mut self, cx: &mut Context<'_>, flow: Flow) -> Poll<io::Result<()>> {
+        loop {
+            let wr_ready = loop {
+                match self.wr_state {
+                    _ if self.tls.is_handshaking() && !self.tls.wants_write() => {
+                        break true;
+                    }
+                    _ if self.tls.is_handshaking() => {}
+                    State::StreamOpen if !self.tls.wants_write() => break true,
+                    State::StreamClosed => {
+                        // Rustls will enqueue the 'CloseNotify' alert and send it after
+                        // flushing the data that is already in the queue.
+                        self.tls.send_close_notify();
+                        self.wr_state = State::TlsClosing;
+                        continue;
+                    }
+                    State::TlsClosing if !self.tls.wants_write() => {
+                        self.wr_state = State::TlsClosed;
+                        continue;
+                    }
+                    // If a 'CloseNotify' alert sent by the remote end has been received,
+                    // shut down the underlying TCP socket. Otherwise, consider polling
+                    // done for the moment.
+                    State::TlsClosed if self.rd_state < State::TlsClosed => break true,
+                    State::TlsClosed if Pin::new(&mut self.tcp).poll_shutdown(cx)?.is_pending() => {
+                        break false;
+                    }
+                    State::TlsClosed => {
+                        self.wr_state = State::TcpClosed;
+                        continue;
+                    }
+                    State::TcpClosed => break true,
+                    _ => {}
+                }
+
+                // Write ciphertext to the TCP socket.
+                let mut wrapped_tcp = ImplementWriteTrait(&mut self.tcp);
+                match self.tls.write_tls(&mut wrapped_tcp) {
+                    Ok(0) => {}        // Wait until the socket has enough buffer space.
+                    Ok(_) => continue, // Try to send more more data immediately.
+                    Err(err) if err.kind() == ErrorKind::WouldBlock => unreachable!(),
+                    Err(err) => return Poll::Ready(Err(err)),
+                }
+
+                // Poll whether there is space in the socket send buffer so we can flush
+                // the remaining outgoing ciphertext.
+                if self.tcp.poll_write_ready(cx)?.is_pending() {
+                    break false;
+                }
+            };
+
+            let rd_ready = loop {
+                // Interpret and decrypt unprocessed TLS protocol data.
+                let tls_state = self
+                    .tls
+                    .process_new_packets()
+                    .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+
+                match self.rd_state {
+                    State::TcpClosed if self.tls.is_handshaking() => {
+                        let err = Error::new(ErrorKind::UnexpectedEof, "tls handshake eof");
+                        return Poll::Ready(Err(err));
+                    }
+                    _ if self.tls.is_handshaking() && !self.tls.wants_read() => {
+                        break true;
+                    }
+                    _ if self.tls.is_handshaking() => {}
+                    State::StreamOpen if tls_state.plaintext_bytes_to_read() > 0 => {
+                        break true;
+                    }
+                    State::StreamOpen if tls_state.peer_has_closed() => {
+                        self.rd_state = State::TlsClosed;
+                        continue;
+                    }
+                    State::StreamOpen => {}
+                    State::StreamClosed if tls_state.plaintext_bytes_to_read() > 0 => {
+                        // Rustls has more incoming cleartext buffered up, but the TLS
+                        // session is closing so this data will never be processed by the
+                        // application layer. Just like what would happen if this were a raw
+                        // TCP stream, don't gracefully end the TLS session, but abort it.
+                        return Poll::Ready(Err(Error::from(ErrorKind::ConnectionReset)));
+                    }
+                    State::StreamClosed => {}
+                    State::TlsClosed if self.wr_state == State::TcpClosed => {
+                        // Keep trying to read from the TCP connection until the remote end
+                        // closes it gracefully.
+                    }
+                    State::TlsClosed => break true,
+                    State::TcpClosed => break true,
+                    _ => unreachable!(),
+                }
+
+                // Try to read more TLS protocol data from the TCP socket.
+                let mut wrapped_tcp = ImplementReadTrait(&mut self.tcp);
+                match self.tls.read_tls(&mut wrapped_tcp) {
+                    Ok(0) => {
+                        self.rd_state = State::TcpClosed;
+                        continue;
+                    }
+                    Ok(_) => continue,
+                    Err(err) if err.kind() == ErrorKind::WouldBlock => {}
+                    Err(err) => return Poll::Ready(Err(err)),
+                }
+
+                // Get notified when more ciphertext becomes available to read from the
+                // TCP socket.
+                if self.tcp.poll_read_ready(cx)?.is_pending() {
+                    break false;
+                }
+            };
+
+            if wr_ready {
+                if self.rd_state >= State::TlsClosed
+                    && self.wr_state >= State::TlsClosed
+                    && self.wr_state < State::TcpClosed
+                {
+                    continue;
+                }
+                if self.tls.wants_write() {
+                    continue;
+                }
+            }
+
+            let io_ready = match flow {
+                _ if self.tls.is_handshaking() => false,
+                Flow::Handshake => true,
+                Flow::Read => rd_ready,
+                Flow::Write => wr_ready,
+            };
+            return match io_ready {
+                false => Poll::Pending,
+                true => Poll::Ready(Ok(())),
+            };
+        }
+    }
+
+    pub(crate) fn poll_handshake(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        if self.tls.is_handshaking() {
+            ready!(self.poll_io(cx, Flow::Handshake))?;
+        }
+        Poll::Ready(Ok(()))
+    }
+
+    pub(crate) fn poll_read(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        ready!(self.poll_io(cx, Flow::Read))?;
+
+        if self.rd_state == State::StreamOpen {
+            // TODO(bartlomieju):
+            #[allow(clippy::undocumented_unsafe_blocks)]
+            let buf_slice = unsafe { &mut *(buf.unfilled_mut() as *mut [_] as *mut [u8]) };
+            let bytes_read = self.tls.reader().read(buf_slice)?;
+            assert_ne!(bytes_read, 0);
+            // TODO(bartlomieju):
+            #[allow(clippy::undocumented_unsafe_blocks)]
+            unsafe {
+                buf.assume_init(bytes_read)
+            };
+            buf.advance(bytes_read);
+        }
+
+        Poll::Ready(Ok(()))
+    }
+
+    pub(crate) fn poll_write(
+        &mut self,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        if buf.is_empty() {
+            // Tokio-rustls compatibility: a zero byte write always succeeds.
+            Poll::Ready(Ok(0))
+        } else if self.wr_state == State::StreamOpen {
+            // Flush Rustls' ciphertext send queue.
+            ready!(self.poll_io(cx, Flow::Write))?;
+
+            // Copy data from `buf` to the Rustls cleartext send queue.
+            let bytes_written = self.tls.writer().write(buf)?;
+            assert_ne!(bytes_written, 0);
+
+            // Try to flush as much ciphertext as possible. However, since we just
+            // handed off at least some bytes to rustls, so we can't return
+            // `Poll::Pending()` any more: this would tell the caller that it should
+            // try to send those bytes again.
+            let _ = self.poll_io(cx, Flow::Write)?;
+
+            Poll::Ready(Ok(bytes_written))
+        } else {
+            // Return error if stream has been shut down for writing.
+            Poll::Ready(Err(ErrorKind::BrokenPipe.into()))
+        }
+    }
+
+    pub(crate) fn poll_shutdown(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        if self.wr_state == State::StreamOpen {
+            self.wr_state = State::StreamClosed;
+        }
+
+        ready!(self.poll_io(cx, Flow::Write))?;
+
+        // At minimum, a TLS 'CloseNotify' alert should have been sent.
+        assert!(self.wr_state >= State::TlsClosed);
+        // If we received a TLS 'CloseNotify' alert from the remote end
+        // already, the TCP socket should be shut down at this point.
+        assert!(self.rd_state < State::TlsClosed || self.wr_state == State::TcpClosed);
+
+        Poll::Ready(Ok(()))
+    }
+
+    pub(crate) fn poll_close(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        if self.rd_state == State::StreamOpen {
+            self.rd_state = State::StreamClosed;
+        }
+
+        // Wait for the handshake to complete.
+        ready!(self.poll_io(cx, Flow::Handshake))?;
+        // Send TLS 'CloseNotify' alert.
+        ready!(self.poll_shutdown(cx))?;
+        // Wait for 'CloseNotify', shut down TCP stream, wait for TCP FIN packet.
+        ready!(self.poll_io(cx, Flow::Read))?;
+
+        assert_eq!(self.rd_state, State::TcpClosed);
+        assert_eq!(self.wr_state, State::TcpClosed);
+
+        Poll::Ready(Ok(()))
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,434 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+use futures::future::poll_fn;
+use futures::task::noop_waker_ref;
+use futures::task::AtomicWaker;
+use futures::task::Context;
+use futures::task::Poll;
+use futures::task::RawWaker;
+use futures::task::RawWakerVTable;
+use futures::task::Waker;
+use tokio::spawn;
+
+use crate::inner::Flow;
+use crate::inner::State;
+use crate::inner::TlsStreamInner;
+use parking_lot::Mutex;
+use rustls::ClientConfig;
+use rustls::ClientConnection;
+use rustls::Connection;
+use rustls::ServerConfig;
+use rustls::ServerConnection;
+use rustls::ServerName;
+use std::io;
+use std::net::SocketAddr;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::sync::Weak;
+use tokio::io::AsyncRead;
+use tokio::io::AsyncWrite;
+use tokio::io::ReadBuf;
+use tokio::net::TcpStream;
+
+mod inner;
+
+pub struct TlsStream(Option<TlsStreamInner>);
+
+impl TlsStream {
+    fn new(tcp: TcpStream, mut tls: Connection) -> Self {
+        tls.set_buffer_limit(None);
+
+        let inner = TlsStreamInner {
+            tcp,
+            tls,
+            rd_state: State::StreamOpen,
+            wr_state: State::StreamOpen,
+        };
+        Self(Some(inner))
+    }
+
+    pub fn new_client_side(
+        tcp: TcpStream,
+        tls_config: Arc<ClientConfig>,
+        server_name: ServerName,
+    ) -> Self {
+        let tls = ClientConnection::new(tls_config, server_name).unwrap();
+        Self::new(tcp, Connection::Client(tls))
+    }
+
+    pub fn new_client_side_from(tcp: TcpStream, connection: ClientConnection) -> Self {
+        Self::new(tcp, Connection::Client(connection))
+    }
+
+    pub fn new_server_side(tcp: TcpStream, tls_config: Arc<ServerConfig>) -> Self {
+        let tls = ServerConnection::new(tls_config).unwrap();
+        Self::new(tcp, Connection::Server(tls))
+    }
+
+    pub fn new_server_side_from(tcp: TcpStream, connection: ServerConnection) -> Self {
+        Self::new(tcp, Connection::Server(connection))
+    }
+
+    pub fn into_split(self) -> (ReadHalf, WriteHalf) {
+        let shared = Shared::new(self);
+        let rd = ReadHalf {
+            shared: shared.clone(),
+        };
+        let wr = WriteHalf { shared };
+        (rd, wr)
+    }
+
+    /// Convenience method to match [`TcpStream`].
+    pub fn peer_addr(&self) -> Result<SocketAddr, io::Error> {
+        self.0.as_ref().unwrap().tcp.peer_addr()
+    }
+
+    /// Convenience method to match [`TcpStream`].
+    pub fn local_addr(&self) -> Result<SocketAddr, io::Error> {
+        self.0.as_ref().unwrap().tcp.local_addr()
+    }
+
+    /// Tokio-rustls compatibility: returns a reference to the underlying TCP
+    /// stream, and a reference to the Rustls `Connection` object.
+    pub fn get_ref(&self) -> (&TcpStream, &Connection) {
+        let inner = self.0.as_ref().unwrap();
+        (&inner.tcp, &inner.tls)
+    }
+
+    fn inner_mut(&mut self) -> &mut TlsStreamInner {
+        self.0.as_mut().unwrap()
+    }
+
+    pub async fn handshake(&mut self) -> io::Result<()> {
+        poll_fn(|cx| self.inner_mut().poll_handshake(cx)).await
+    }
+
+    fn poll_handshake(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.inner_mut().poll_handshake(cx)
+    }
+
+    pub fn get_alpn_protocol(&mut self) -> Option<&[u8]> {
+        self.inner_mut().tls.alpn_protocol()
+    }
+}
+
+impl AsyncRead for TlsStream {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        self.inner_mut().poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for TlsStream {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        self.inner_mut().poll_write(cx, buf)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.inner_mut().poll_io(cx, Flow::Write)
+        // The underlying TCP stream does not need to be flushed.
+    }
+
+    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.inner_mut().poll_shutdown(cx)
+    }
+}
+
+impl Drop for TlsStream {
+    fn drop(&mut self) {
+        let mut inner = self.0.take().unwrap();
+
+        // If read and write are closed, we can fast exit here
+        if inner.wr_state != State::StreamOpen && inner.rd_state != State::StreamOpen {
+            return;
+        }
+
+        let mut cx = Context::from_waker(noop_waker_ref());
+        let use_linger_task = inner.poll_close(&mut cx).is_pending();
+
+        if use_linger_task {
+            spawn(poll_fn(move |cx: &mut Context| inner.poll_close(cx)));
+        } else if cfg!(debug_assertions) {
+            spawn(async {}); // Spawn dummy task to detect missing LocalSet.
+        }
+    }
+}
+
+pub struct ReadHalf {
+    shared: Arc<Shared>,
+}
+
+impl ReadHalf {
+    pub fn reunite(self, wr: WriteHalf) -> TlsStream {
+        assert!(Arc::ptr_eq(&self.shared, &wr.shared));
+        drop(wr); // Drop `wr`, so only one strong reference to `shared` remains.
+
+        Arc::try_unwrap(self.shared)
+            .unwrap_or_else(|_| panic!("Arc::<Shared>::try_unwrap() failed"))
+            .tls_stream
+            .into_inner()
+    }
+}
+
+impl AsyncRead for ReadHalf {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        self.shared
+            .poll_with_shared_waker(cx, Flow::Read, move |tls, cx| tls.poll_read(cx, buf))
+    }
+}
+
+pub struct WriteHalf {
+    shared: Arc<Shared>,
+}
+
+impl WriteHalf {
+    pub async fn handshake(&mut self) -> io::Result<()> {
+        poll_fn(|cx| {
+            self.shared
+                .poll_with_shared_waker(cx, Flow::Write, |mut tls, cx| tls.poll_handshake(cx))
+        })
+        .await
+    }
+}
+
+impl AsyncWrite for WriteHalf {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        self.shared
+            .poll_with_shared_waker(cx, Flow::Write, move |tls, cx| tls.poll_write(cx, buf))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.shared
+            .poll_with_shared_waker(cx, Flow::Write, |tls, cx| tls.poll_flush(cx))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.shared
+            .poll_with_shared_waker(cx, Flow::Write, |tls, cx| tls.poll_shutdown(cx))
+    }
+}
+
+struct Shared {
+    tls_stream: Mutex<TlsStream>,
+    rd_waker: AtomicWaker,
+    wr_waker: AtomicWaker,
+}
+
+impl Shared {
+    fn new(tls_stream: TlsStream) -> Arc<Self> {
+        let self_ = Self {
+            tls_stream: Mutex::new(tls_stream),
+            rd_waker: AtomicWaker::new(),
+            wr_waker: AtomicWaker::new(),
+        };
+        Arc::new(self_)
+    }
+
+    fn poll_with_shared_waker<R>(
+        self: &Arc<Self>,
+        cx: &mut Context<'_>,
+        flow: Flow,
+        mut f: impl FnMut(Pin<&mut TlsStream>, &mut Context<'_>) -> R,
+    ) -> R {
+        match flow {
+            Flow::Handshake => unreachable!(),
+            Flow::Read => self.rd_waker.register(cx.waker()),
+            Flow::Write => self.wr_waker.register(cx.waker()),
+        }
+
+        let shared_waker = self.new_shared_waker();
+        let mut cx = Context::from_waker(&shared_waker);
+
+        let mut tls_stream = self.tls_stream.lock();
+        f(Pin::new(&mut tls_stream), &mut cx)
+    }
+
+    const SHARED_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(
+        Self::clone_shared_waker,
+        Self::wake_shared_waker,
+        Self::wake_shared_waker_by_ref,
+        Self::drop_shared_waker,
+    );
+
+    fn new_shared_waker(self: &Arc<Self>) -> Waker {
+        let self_weak = Arc::downgrade(self);
+        let self_ptr = self_weak.into_raw() as *const ();
+        let raw_waker = RawWaker::new(self_ptr, &Self::SHARED_WAKER_VTABLE);
+        // TODO(bartlomieju):
+        #[allow(clippy::undocumented_unsafe_blocks)]
+        unsafe {
+            Waker::from_raw(raw_waker)
+        }
+    }
+
+    fn clone_shared_waker(self_ptr: *const ()) -> RawWaker {
+        // TODO(bartlomieju):
+        #[allow(clippy::undocumented_unsafe_blocks)]
+        let self_weak = unsafe { Weak::from_raw(self_ptr as *const Self) };
+        let ptr1 = self_weak.clone().into_raw();
+        let ptr2 = self_weak.into_raw();
+        assert!(ptr1 == ptr2);
+        RawWaker::new(self_ptr, &Self::SHARED_WAKER_VTABLE)
+    }
+
+    fn wake_shared_waker(self_ptr: *const ()) {
+        Self::wake_shared_waker_by_ref(self_ptr);
+        Self::drop_shared_waker(self_ptr);
+    }
+
+    fn wake_shared_waker_by_ref(self_ptr: *const ()) {
+        // TODO(bartlomieju):
+        #[allow(clippy::undocumented_unsafe_blocks)]
+        let self_weak = unsafe { Weak::from_raw(self_ptr as *const Self) };
+        if let Some(self_arc) = Weak::upgrade(&self_weak) {
+            self_arc.rd_waker.wake();
+            self_arc.wr_waker.wake();
+        }
+        let _ = self_weak.into_raw();
+    }
+
+    fn drop_shared_waker(self_ptr: *const ()) {
+        // TODO(bartlomieju):
+        #[allow(clippy::undocumented_unsafe_blocks)]
+        let _ = unsafe { Weak::from_raw(self_ptr as *const Self) };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rustls::{
+        client::{ServerCertVerified, ServerCertVerifier},
+        Certificate, PrivateKey,
+    };
+    use std::{
+        io::BufRead,
+        net::{Ipv4Addr, SocketAddrV4},
+    };
+    use tokio::{
+        io::{AsyncReadExt, AsyncWriteExt},
+        net::{TcpListener, TcpSocket},
+        spawn,
+    };
+
+    struct UnsafeVerifier {}
+
+    impl ServerCertVerifier for UnsafeVerifier {
+        fn verify_server_cert(
+            &self,
+            _end_entity: &Certificate,
+            _intermediates: &[Certificate],
+            _server_name: &ServerName,
+            _scts: &mut dyn Iterator<Item = &[u8]>,
+            _ocsp_response: &[u8],
+            _now: std::time::SystemTime,
+        ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+            Ok(ServerCertVerified::assertion())
+        }
+    }
+
+    fn certificate() -> Certificate {
+        let buf_read: &mut dyn BufRead = &mut &include_bytes!("testdata/localhost.crt")[..];
+        let cert = rustls_pemfile::read_one(buf_read)
+            .expect("Failed to load test cert")
+            .unwrap();
+        match cert {
+            rustls_pemfile::Item::X509Certificate(cert) => Certificate(cert),
+            _ => {
+                panic!("Unexpected item")
+            }
+        }
+    }
+
+    fn private_key() -> PrivateKey {
+        let buf_read: &mut dyn BufRead = &mut &include_bytes!("testdata/localhost.key")[..];
+        let cert = rustls_pemfile::read_one(buf_read)
+            .expect("Failed to load test key")
+            .unwrap();
+        match cert {
+            rustls_pemfile::Item::PKCS8Key(key) => PrivateKey(key),
+            _ => {
+                panic!("Unexpected item")
+            }
+        }
+    }
+
+    fn server_config() -> ServerConfig {
+        ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(vec![certificate()], private_key())
+            .expect("Failed to build server config")
+    }
+
+    fn client_config() -> ClientConfig {
+        ClientConfig::builder()
+            .with_safe_defaults()
+            .with_custom_certificate_verifier(Arc::new(UnsafeVerifier {}))
+            .with_no_client_auth()
+    }
+
+    async fn tcp_pair() -> (TcpStream, TcpStream) {
+        let listener = TcpListener::bind(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0)))
+            .await
+            .unwrap();
+        let port = listener.local_addr().unwrap().port();
+        let server = spawn(async move { listener.accept().await.unwrap().0 });
+        let client = spawn(async move {
+            TcpSocket::new_v4()
+                .unwrap()
+                .connect(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, port)))
+                .await
+                .unwrap()
+        });
+
+        let (server, client) = (server.await.unwrap(), client.await.unwrap());
+        (server, client)
+    }
+
+    async fn tls_pair() -> (TlsStream, TlsStream) {
+        let (server, client) = tcp_pair().await;
+        let server = TlsStream::new_server_side(server, server_config().into());
+        let client = TlsStream::new_client_side(
+            client,
+            client_config().into(),
+            "example.com".try_into().unwrap(),
+        );
+
+        (server, client)
+    }
+
+    #[tokio::test]
+    async fn test_client_server() -> Result<(), Box<dyn std::error::Error>> {
+        let (mut server, mut client) = tls_pair().await;
+        spawn(async move {
+            server.write_all(b"hello?").await.unwrap();
+            let mut buf = [0; 6];
+            server.read_exact(&mut buf).await.unwrap();
+            assert_eq!(buf.as_slice(), b"hello!");
+        });
+        spawn(async move {
+            client.write_all(b"hello!").await.unwrap();
+            let mut buf = [0; 6];
+            client.read_exact(&mut buf).await.unwrap();
+            assert_eq!(buf.as_slice(), b"hello?");
+        });
+
+        Ok(())
+    }
+}
